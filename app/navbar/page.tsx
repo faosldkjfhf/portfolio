500c7fbbc926f2356cd19fcc17237cd2b8facd3b
Kevin Liu <kliu0932@gmail.com>
2024-04-26 14:10:06 -0400

    basic layout

---
+++ app/navbar/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import "./page.css";
+
+type IProps = {
+  position: String;
+};
+
+export default function NavBar(): React.ReactElement {
+  return <div></div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
-import Image from "next/image";
-import "./page.css";
+"use client";
 import Link from "next/link";
+import "./page.css";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home(): React.ReactElement {
+  const [defaultPos, setDefaultPos] = useState(true);
   return (
     <main className="container mx-auto">
-      <div className="center">
-        <div className="text-8xl/10 font-bold">Kevin Liu</div>
-        <nav className="flex-row mt-10 space-x-5">
-          <Link href="/">About</Link>
-          <Link href="/">Projects</Link>
-          <Link href="/">Photos</Link>
+      <div className={`${defaultPos ? "center" : "top"}`}>
+        <div
+          className="text-[8rem]/10 font-bold button cursor-pointer"
+          onClick={() => {
+            setDefaultPos(true);
+          }}
+        >
+          Kevin Liu
+        </div>
+        <nav className="mt-12 font-bold text-2xl flex-row space-x-5 justify-start">
+          <Link
+            className="button"
+            href="/"
+            onClick={() => {
+              setDefaultPos(false);
+            }}
+          >
+            About
+          </Link>
+          <Link className="button" href="/">
+            Projects
+          </Link>
+          <Link className="button" href="/">
+            Photos
+          </Link>
         </nav>
       </div>
     </main>
